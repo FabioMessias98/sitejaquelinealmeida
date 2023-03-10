57c23a344786644431e7928d9360ecc50fc5f60a
Gustavo Santos <gustavo.rodrigues35@fatec.sp.gov.br>
2023-03-10 12:04:09 -0300

    terminando contact us

--- themes/theme-evolutap/page-contact.php
+++ themes/theme-evolutap/page-contact.php
@@ -68,7 +68,7 @@ while ( have_posts() ) : the_post(); ?>
 <!-- end banner social -->
 
 <!-- contact -->
-<section class="pg-contact mt-10 mb-5">
+<section class="feature pg-contact mt-10 mb-5">
 
 	<div class="container">
 		
@@ -81,15 +81,15 @@ while ( have_posts() ) : the_post(); ?>
 					<div class="form-row flex-column">
 						
 						<div class="d-flex flex-md-row flex-column">
-							<div class="col-xl-6 col-md-5 col-12 mt-2 pl-1">
-								<label>First Name</label>
+							<div class="col-xl-6 col-md-6 col-12 mt-2 pl-1 pr-md-5 pr-0">
+								<label class="color-folk--purple u-font-family-Bahnschrift u-font-size-26 font-weight-bold">First Name</label>
 								<input
 								class="contact--input--message-new-bg-color page-contact form-control border-0 rounded-0 box-shadow-folk pl-4 mb-lg-0 mb-3"
 								type="text"
 								id="firstName">
 							</div>
-							<div class="col-xl-6 col-md-5 col-12 mt-2 px-md-1 pl-1">
-								<label>Last Name</label>
+							<div class="col-xl-6 col-md-6 col-12 mt-2 px-md-1 pl-1 pr-0">
+								<label class="color-folk--purple u-font-family-Bahnschrift u-font-size-26 font-weight-bold">Last Name</label>
 								<input
 								class="contact--input--message-new-bg-color form-control border-0 rounded-0 box-shadow-folk pl-4"
 								type="text"
@@ -97,24 +97,24 @@ while ( have_posts() ) : the_post(); ?>
 							</div>
 						</div>
 
-						<div class="col-xl-12 col-md-10 col-12 mt-4 px-md-1 pr-3">
-							<label>Email</label>
+						<div class="col-xl-12 col-12 mt-4 px-md-1">
+							<label class="color-folk--purple u-font-family-Bahnschrift u-font-size-26 font-weight-bold">Email</label>
 							<input 
 							class="contact--input--message-new-bg-color form-control border-0 rounded-0 box-shadow-folk pl-4"
 							type="email"
 							id="email">
 						</div>
 
-						<div class="d-flex flex-row align-items-center">
-							<div class="col-xl-6 col-md-10 col-12 mt-4 px-md-1 pr-3">
-								<label>Phone</label>
+						<div class="d-flex flex-lg-row flex-md-column align-items-center">
+							<div class="col-xl-6 col-lg-6 col-12 mt-4 pl-1 pr-lg-5 pr-0">
+								<label class="color-folk--purple u-font-family-Bahnschrift u-font-size-26 font-weight-bold">Phone</label>
 								<input
 								class="contact--input--message-new-bg-color form-control border-0 rounded-0 box-shadow-folk pl-4"
 								type="tel"
 								id="phone">
 							</div>
 
-							<div class="col-xl-6 col-md-10 col-12 mt-20 px-md-1 pr-3">
+							<div class="col-xl-6 col-md-12 col-lg-6 col-12 mt-20 px-md-1">
 								<select class="form-control border-0 rounded-0 box-shadow-folk" style="padding: 2rem 0.5rem;"> 
 									<option selected>What would you like to know </option>
 									<option>Mind&Body Defense Programme</option>
@@ -125,7 +125,14 @@ while ( have_posts() ) : the_post(); ?>
 							</div>
 						</div>
 
-						<div class="col-xl-12 col-md-10	 mt-5">
+						<div class="d-flex flex-column">
+							<div class="col-xl-12 col-12 mt-4 px-0">
+								<label class="color-folk--purple u-font-family-Bahnschrift u-font-size-26 font-weight-bold">Message</label>
+								<textarea class="w-100" id="" cols="30" rows="10"></textarea>
+							</div>
+						</div>
+
+						<div class="col-xl-12 col-12 mt-5">
 							<input
 							class="btn contact--submit text-white bg-folk--light-purple font-size-22 rounded-sm d-block w-100 p-3"
 							type="submit"
